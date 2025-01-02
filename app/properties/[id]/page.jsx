7c97543f820165ff3bccff1543287215cb10e9a9
Ryan Lee <ryanjunghyunlee@naver.com>
2025-01-02 20:45:21 +0900

    Add property header and back button

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,10 +1,33 @@
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
+
 const PropertyPage = async ({ params }) => {
   const { id } = await params;
+  await connectDB();
+  const property = await Property.findById(id).lean();
 
   return (
-    <div>
-      <h1>Property Page {id}</h1>
-    </div>
+    <>
+      <PropertyHeaderImage image={property.images[0]} />
+
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            href="/properties"
+            className="text-blue-500 hover:text-blue-600 flex items-center"
+          >
+            <FaArrowLeft className="mr-2" /> Back to Properties
+          </Link>
+        </div>
+      </section>
+
+      <section>
+        <p>{property.name}</p>
+      </section>
+    </>
   );
 };
 
